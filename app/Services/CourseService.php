3192f1569d8b2be4e53a1097805f12ec81098d49
remonwaasef <reemmoo20022@gmail.com>
2026-02-03 19:47:26 +0200

    Final stability fix for course updates and localized day mapping

--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -165,8 +165,8 @@ class CourseService
         $dayMapping = [
             'sunday' => 0, 'monday' => 1, 'tuesday' => 2, 'wednesday' => 3,
             'thursday' => 4, 'friday' => 5, 'saturday' => 6,
-            'Sunday' => 0, 'Monday' => 1, 'Tuesday' => 2, 'Wednesday' => 3,
-            'Thursday' => 4, 'Friday' => 5, 'Saturday' => 6,
+            'الأحد' => 0, 'الاثنين' => 1, 'الثلاثاء' => 2, 'الأربعاء' => 3,
+            'الخميس' => 4, 'الجمعة' => 5, 'السبت' => 6,
         ];
 
         $schedules = [];
