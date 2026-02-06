7b6bd0732160a11c351e662f60be0a77a9229106
remonwaasef <reemmoo20022@gmail.com>
2026-02-06 11:22:12 +0200

    Auto-deploy 2026-02-06 11:22

---
+++ Modules/Center/resources/lang/en/expenses.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'title' => 'Expenses',
+    'new_expense' => 'Record New Expense',
+    'edit_expense' => 'Edit Expense',
+    'category' => 'Category',
+    'amount' => 'Amount',
+    'date' => 'Date',
+    'description' => 'Description',
+    'payment_method' => 'Payment Method',
+    'attachment' => 'Attachment',
+    'created_by' => 'Created By',
+    'action' => 'Actions',
+    'currency' => 'EGP',
+    'no_expenses' => 'No expenses recorded currently.',
+    'select_category' => 'Select Category',
+    'all_categories' => 'All Categories',
+    'filter' => 'Filter',
+    'start_date' => 'From Date',
+    'end_date' => 'To Date',
+];
---
+++ Modules/Center/resources/lang/fr/expenses.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'title' => 'Dépenses',
+    'new_expense' => 'Enregistrer une Dépense',
+    'edit_expense' => 'Modifier la Dépense',
+    'category' => 'Catégorie',
+    'amount' => 'Montant',
+    'date' => 'Date',
+    'description' => 'Description',
+    'payment_method' => 'Méthode de Paiement',
+    'attachment' => 'Pièce Jointe',
+    'created_by' => 'Créé Par',
+    'action' => 'Actions',
+    'currency' => 'EGP',
+    'no_expenses' => 'Aucune dépense enregistrée pour le moment.',
+    'select_category' => 'Sélectionner la Catégorie',
+    'all_categories' => 'Toutes les Catégories',
+    'filter' => 'Filtrer',
+    'start_date' => 'Date de Début',
+    'end_date' => 'Date de Fin',
+];
