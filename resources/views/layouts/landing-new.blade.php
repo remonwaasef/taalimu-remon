98a4c962bddc2f9bcb1418f85596d0fa5feaebc4
remonwaasef <reemmoo20022@gmail.com>
2026-02-11 02:35:47 +0200

    Auto-deploy 2026-02-11 02:35

--- resources/views/layouts/landing-new.blade.php
+++ resources/views/layouts/landing-new.blade.php
@@ -170,7 +170,7 @@
     <script>
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js?v=' + Date.now())
+                navigator.serviceWorker.register('/service-worker.js')
                     .then((reg) => console.log('SW registered:', reg.scope))
                     .catch((err) => console.log('SW failed:', err));
             });
