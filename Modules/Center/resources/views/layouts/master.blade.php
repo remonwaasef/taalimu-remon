444e1b555395cff2d1222b5720c1511ba7e4d236
remonwaasef <reemmoo20022@gmail.com>
2026-02-13 17:00:01 +0200

    Auto-deploy 2026-02-13 17:00

--- Modules/Center/resources/views/layouts/master.blade.php
+++ Modules/Center/resources/views/layouts/master.blade.php
@@ -28,6 +28,9 @@
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
+    <!-- Bootstrap Bundle JS (needed for Sidebar/Modals) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <!-- SweetAlert2 -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
