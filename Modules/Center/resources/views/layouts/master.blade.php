3857c58315714b08cec0cbb61eb0af4ca9da702c
remonwaasef <reemmoo20022@gmail.com>
2026-02-14 02:18:22 +0200

    Auto-deploy 2026-02-14 02:18

--- Modules/Center/resources/views/layouts/master.blade.php
+++ Modules/Center/resources/views/layouts/master.blade.php
@@ -28,9 +28,6 @@
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
-    <!-- Bootstrap Bundle JS (needed for Sidebar/Modals) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
     <!-- SweetAlert2 -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
@@ -41,7 +38,7 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="apple-mobile-web-app-title" content="Taalimu">
     
-    @vite(['resources/css/app.scss', 'resources/js/app.js'])
+    
     
     <style>
         :root {
@@ -786,6 +783,7 @@
         });
     </script>
     
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
     @stack('scripts')
 </body>
 </html>
