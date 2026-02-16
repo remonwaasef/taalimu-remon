e5cebe5be39471087548fe488eb588372c6b7c25
remonwaasef <reemmoo20022@gmail.com>
2026-02-16 04:04:43 +0200

    Auto-deploy 2026-02-16 04:04

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,16 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Load all console commands from app/Console/Commands
+$commandDir = app_path('Console/Commands');
+if (is_dir($commandDir)) {
+    foreach (glob($commandDir . '/*.php') as $filename) {
+        $class = 'App\\Console\\Commands\\' . basename($filename, '.php');
+        if (class_exists($class)) {
+            Artisan::starting(function ($artisan) use ($class) {
+                $artisan->resolve($class);
+            });
+        }
+    }
+}
