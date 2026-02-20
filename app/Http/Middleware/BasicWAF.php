07b8afe9209ee4decd0c65f1c0f236c1983f2cce
remonwaasef <reemmoo20022@gmail.com>
2026-02-20 06:18:41 +0200

    Auto-deploy 2026-02-20 06:18

--- app/Http/Middleware/BasicWAF.php
+++ app/Http/Middleware/BasicWAF.php
@@ -20,7 +20,7 @@ class BasicWAF
         // XSS
         '/<script[^>]*>.*?<\/script>/i',
         '/javascript:/i',
-        '/on\w+\s*=/i',
+        '/(?:\s|["\'\/>])on\w+\s*=/i',
         
         // Path Traversal
         '/\.\.[\/\\\\]/',
