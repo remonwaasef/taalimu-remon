efc7beef68c0c0b1b4e71406a87422a221d42e66
remonwaasef <reemmoo20022@gmail.com>
2026-02-20 05:07:27 +0200

    Auto-deploy 2026-02-20 05:07

--- Modules/Campus/resources/views/layouts/master.blade.php
+++ Modules/Campus/resources/views/layouts/master.blade.php
@@ -14,6 +14,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <!-- Font Awesome (Required for icons in index) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!-- PWA Support -->
     <link rel="manifest" href="/manifest.json">
