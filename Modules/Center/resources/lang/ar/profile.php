326a4591fc1f7308cb507651eff29d90af2cf358
remonwaasef <reemmoo20022@gmail.com>
2026-02-23 06:25:12 +0200

    Auto-deploy 2026-02-23 06:25

---
+++ Modules/Center/resources/lang/ar/profile.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'update_profile_description' => 'تحديث البيانات الشخصية وكلمة المرور الخاصة بك',
+    'full_name' => 'الاسم بالكامل',
+    'email' => 'البريد الإلكتروني',
+    'change_password' => 'تغيير كلمة المرور',
+    'new_password' => 'كلمة المرور الجديدة',
+    'confirm_password' => 'تأكيد كلمة المرور الجديدة',
+    'save_changes' => 'حفظ التعديلات',
+    'system_language' => 'لغة النظام',
+    'language_description' => 'اختر لغتك المفضلة لعرض لوحة التحكم',
+    'arabic' => 'العربية',
+    'english' => 'English',
+    'french' => 'Français',
+];
