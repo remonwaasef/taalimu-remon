745465af954668dd034be100a10691a6a3a8546c
remonwaasef <reemmoo20022@gmail.com>
2026-02-24 08:38:33 +0200

    Fix for 429 Too Many Requests error\n

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,7 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\ContentSecurityPolicy::class,
             \App\Http\Middleware\BasicWAF::class,
             \App\Http\Middleware\PaginationLimit::class,
-            'throttle:60,1',
+            'throttle:300,1',
         ]);
 
         $middleware->api(append: [
