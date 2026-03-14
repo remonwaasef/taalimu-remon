13c1b59d3b9dfdbe0272912b3ea64502f4544a69
Remon <remon@example.com>
2026-03-14 03:24:39 +0200

    Auto-deploy 2026-03-14 03:24

--- app/Http/Controllers/PayPalWebhookController.php
+++ app/Http/Controllers/PayPalWebhookController.php
@@ -83,7 +83,7 @@ class PayPalWebhookController extends Controller
                 // Simplification for now
                 $daysToAdd = 30;
                 if ($subscription->billing_cycle === 'term') {
-                    $termDuration = \App\Models\SiteSetting::get('term_duration_days', 150);
+                    $termDuration = (int) \App\Models\SiteSetting::get('term_duration_days', 150);
                     $daysToAdd = $termDuration;
                 } elseif ($subscription->billing_cycle === 'yearly') {
                     $daysToAdd = 365;
