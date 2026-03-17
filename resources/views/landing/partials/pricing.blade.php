4fcd9d210aa1194618d92a1f02615d7803f3a5ff
Remon <remon@example.com>
2026-03-17 03:14:27 +0200

    Auto-deploy 2026-03-17 03:14

--- resources/views/landing/partials/pricing.blade.php
+++ resources/views/landing/partials/pricing.blade.php
@@ -1,4 +1,4 @@
-<section id="pricing" class="py-12 lg:py-16 bg-slate-50/80 border-y border-border/40" 
+<section id="pricing" class="py-16 lg:py-24 bg-slate-100 border-y border-slate-200" 
          x-data="{ 
              billingCycle: 'monthly',
             userCountry: 'default',
