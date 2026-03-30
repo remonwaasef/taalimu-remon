b6d833b677dc30acd38b5b17f1fe4447c1f6f467
Remon <remon@example.com>
2026-03-30 08:09:02 +0200

    Auto-deploy 2026-03-30 08:09

--- Modules/Admin/routes/api.php
+++ Modules/Admin/routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Admin\Http\Controllers\AdminController;
 
-Route::middleware(['auth:sanctum', 'role:super_admin'])->prefix('v1')->group(function () {
+Route::middleware(['auth:sanctum', \App\Http\Middleware\CheckAdminRole::class])->prefix('v1')->group(function () {
     Route::apiResource('admins', AdminController::class)->names('admin');
 });
