fea9991ddcebc4da9a3cad6f3816ab878d124b1c
Remon <remon@example.com>
2026-03-30 08:30:27 +0200

    Auto-deploy 2026-03-30 08:30

--- Modules/Admin/resources/views/settings/index.blade.php
+++ Modules/Admin/resources/views/settings/index.blade.php
@@ -1,4 +1,4 @@
-@extends('admin::layouts.hope-master')
+@extends('admin::layouts.master')
 
 @section('title', __('admin::admin.title'))
 @section('page-title', __('admin::admin.title'))
--- Modules/Admin/resources/views/tickets/index.blade.php
+++ Modules/Admin/resources/views/tickets/index.blade.php
@@ -1,4 +1,4 @@
-@extends('admin::layouts.hope-master')
+@extends('admin::layouts.master')
 
 @section('title', __('admin.tickets.title'))
 
