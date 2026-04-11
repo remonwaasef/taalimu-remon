8f655c8ec8aa5bb95518a020468bc79e679d0417
Remon <remon@example.com>
2026-04-11 03:29:10 +0200

    Auto-deploy 2026-04-11 03:29

--- database/migrations/2026_04_11_000000_create_online_classes_table.php
+++ database/migrations/2026_04_11_000000_create_online_classes_table.php
@@ -11,6 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('online_classes');
+        
         Schema::create('online_classes', function (Blueprint $table) {
             $table->id();
             
