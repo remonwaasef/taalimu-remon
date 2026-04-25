f0bb845d09f6c43ada301cd30019fcd38b70d769
Remon <remon@example.com>
2026-04-25 22:00:06 +0200

    Auto-deploy 2026-04-25 22:00

---
+++ scratch/check_fr.php
@@ -0,0 +1,19 @@
+<?php
+$arFiles = glob('Modules/Center/resources/lang/ar/*.php');
+foreach ($arFiles as $arFile) {
+    $basename = basename($arFile);
+    $frFile = 'Modules/Center/resources/lang/fr/' . $basename;
+    if (file_exists($frFile)) {
+        $ar = require $arFile;
+        $fr = require $frFile;
+        if (is_array($ar) && is_array($fr)) {
+            $missing = array_diff_key($ar, $fr);
+            if (!empty($missing)) {
+                echo "Missing in $basename:\n";
+                print_r(array_keys($missing));
+            }
+        }
+    } else {
+        echo "Missing file: $basename in fr/\n";
+    }
+}
