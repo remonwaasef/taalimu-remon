a5c6453093822443ae4ea57ccd5ab6c1f8606f7f
Remon <remon@example.com>
2026-04-25 21:14:19 +0200

    fix: update UserPolicy and RolePolicy to use correct manage users permission

--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -9,7 +9,7 @@ class RolePolicy
 {
     public function viewAny(User $user): bool
     {
-        return $user->hasAnyRole(['center_admin', 'admin']) || $user->hasPermissionTo('view roles');
+        return $user->hasAnyRole(['center_admin', 'admin']) || $user->hasPermissionTo('manage users');
     }
 
     public function view(User $user, Role $role): bool
@@ -19,20 +19,20 @@ class RolePolicy
 
     public function create(User $user): bool
     {
-        return $user->hasAnyRole(['center_admin', 'admin']) || $user->hasPermissionTo('create roles');
+        return $user->hasAnyRole(['center_admin', 'admin']) || $user->hasPermissionTo('manage users');
     }
 
     public function update(User $user, Role $role): bool
     {
         return !is_null($role->tenant_id) && 
                $user->tenant_id === $role->tenant_id && 
-               ($user->hasAnyRole(['center_admin', 'admin']) || $user->hasPermissionTo('update roles'));
+               ($user->hasAnyRole(['center_admin', 'admin']) || $user->hasPermissionTo('manage users'));
     }
 
     public function delete(User $user, Role $role): bool
     {
         return !is_null($role->tenant_id) && 
                $user->tenant_id === $role->tenant_id && 
-               ($user->hasAnyRole(['center_admin', 'admin']) || $user->hasPermissionTo('delete roles'));
+               ($user->hasAnyRole(['center_admin', 'admin']) || $user->hasPermissionTo('manage users'));
     }
 }
