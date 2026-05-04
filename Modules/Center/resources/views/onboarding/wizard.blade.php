8c45b3ee7be415f57a0625982a420c625d9e2f9a
Remon <remon@example.com>
2026-05-05 00:04:55 +0200

    Auto-deploy 2026-05-05 00:04

--- Modules/Center/resources/views/onboarding/wizard.blade.php
+++ Modules/Center/resources/views/onboarding/wizard.blade.php
@@ -14,7 +14,7 @@
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
-    @vite(['resources/css/landing-new.css'])
+    <link rel="stylesheet" href="{{ asset('css/landing-new.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/intersect@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
