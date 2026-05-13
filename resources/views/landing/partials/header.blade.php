fbf9b9a03f84673ce23130ded54b0b6fc7d1d57a
Remon <remon@example.com>
2026-05-14 00:06:51 +0200

    Auto-deploy 2026-05-14 00:06

--- resources/views/landing/partials/header.blade.php
+++ resources/views/landing/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header 
-    class="fixed top-0 left-0 right-0 z-50 transition-all duration-300"
+    class="fixed top-0 left-0 right-0 z-[100] transition-all duration-300"
     x-data="{ 
         scrolled: false,
         isMenuOpen: false
