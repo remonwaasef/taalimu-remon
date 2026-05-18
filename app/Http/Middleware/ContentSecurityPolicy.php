72e5669bf7acda297efec4cade0306c8d4e16be8
Taalimu_Admin <admin@taalimu.com>
2026-05-18 15:16:45 +0300

    Auto-deploy 2026-05-18 15:16

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -43,7 +43,6 @@ class ContentSecurityPolicy
                 "frame-ancestors 'none'",
                 "frame-src 'self' https://www.youtube.com https://player.vimeo.com https://js.stripe.com",
                 "base-uri 'self'",
-                "form-action 'self' https: https://checkout.stripe.com",
             ];
         }
 
