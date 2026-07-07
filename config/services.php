1056323d7a0f5c733ec0680508131380c68d5011
Remon <remon@example.com>
2026-07-07 20:53:14 +0200

    Auto-deploy 2026-07-07 20:53

--- config/services.php
+++ config/services.php
@@ -62,6 +62,7 @@ return [
         'client_id' => env('PAYPAL_CLIENT_ID'),
         'client_secret' => env('PAYPAL_CLIENT_SECRET'),
         'mode' => env('PAYPAL_MODE', 'sandbox'),
+        'webhook_id' => env('PAYPAL_WEBHOOK_ID'),
     ],
 
     'paymob' => [
