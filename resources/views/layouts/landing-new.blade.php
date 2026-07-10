d7bc7e8deeba43139050fadf56bfe880aa56f877
Remon Wasef <remonw@example.com>
2026-07-11 00:34:01 +0200

    Auto-deploy 2026-07-11 00:34

--- resources/views/layouts/landing-new.blade.php
+++ resources/views/layouts/landing-new.blade.php
@@ -149,7 +149,7 @@
 
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/service-worker.js?v=9')
+                navigator.serviceWorker.register('/service-worker.js?v=11')
                     .then((reg) => console.log('SW registered:', reg.scope))
                     .catch((err) => console.log('SW failed:', err));
             });
