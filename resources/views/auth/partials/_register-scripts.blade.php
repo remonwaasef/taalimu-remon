5f54516f76f61947715a3424ec3faf424a831ee6
Remon Wasef <remonw@example.com>
2026-07-10 23:25:23 +0200

    Auto-deploy 2026-07-10 23:25

--- resources/views/auth/partials/_register-scripts.blade.php
+++ resources/views/auth/partials/_register-scripts.blade.php
@@ -1,4 +1,4 @@
-<script>
+<script nonce="{{ $csp_nonce ?? '' }}">
 document.addEventListener('alpine:init', () => {
     Alpine.data('registrationForm', (config) => ({
         selectedPlan: config.selectedPlan,
