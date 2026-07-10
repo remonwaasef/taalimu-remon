bccf81b9208cb889a59a0f282b8fcb8f055bf414
Remon Wasef <remonw@example.com>
2026-07-11 00:33:30 +0200

    Auto-deploy 2026-07-11 00:33

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -26,7 +26,7 @@ class ContentSecurityPolicy
             $csp = [
                 "default-src 'self' {$cdn} {$fonts}",
                 "script-src 'self' 'nonce-{$nonce}' {$cdn}",
-                "style-src 'self' 'nonce-{$nonce}' {$cdn} {$fonts}",
+                "style-src 'self' 'unsafe-inline' {$cdn} {$fonts}",
                 "font-src 'self' data: {$cdn} {$fonts}",
                 "img-src 'self' data: blob: https:",
                 "connect-src 'self' ws: wss:",
@@ -42,7 +42,7 @@ class ContentSecurityPolicy
             $csp = [
                 "default-src 'self'",
                 "script-src 'self' 'nonce-{$nonce}' {$cdn}",
-                "style-src 'self' 'unsafe-inline' 'nonce-{$nonce}' {$cdn} {$fonts}",
+                "style-src 'self' 'unsafe-inline' {$cdn} {$fonts}",
                 "font-src 'self' data: {$cdn} {$fonts}",
                 "img-src 'self' data: blob: https:",
                 "connect-src 'self'",
