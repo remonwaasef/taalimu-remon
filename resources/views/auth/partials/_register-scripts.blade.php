f75c73947675f880d13f8687d7c8a6f7910c7f99
Remon <remon@example.com>
2026-07-24 14:33:28 +0200

    Auto-deploy 2026-07-24 14:33

--- resources/views/auth/partials/_register-scripts.blade.php
+++ resources/views/auth/partials/_register-scripts.blade.php
@@ -23,7 +23,7 @@ document.addEventListener('alpine:init', () => {
         discountText: '',
         isApplyingCoupon: false,
         selectedCurrency: config.selectedCurrency || 'EGP',
-        accountType: config.accountType || 'instructor',
+        accountType: config.accountType || null,
         showPlanModal: false,
         formSubmitted: false,
         phoneVerified: false,
