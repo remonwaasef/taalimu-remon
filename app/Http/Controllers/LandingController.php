6f0be7eb4f9af25ce074289e983770ac26f6c176
Remon <remon@example.com>
2026-08-28 05:45:55 +0200

    refactor: restore polished official landing page (landing.new) with Tailwind design system

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -29,8 +29,8 @@ class LandingController extends Controller
         }
 
         // Set Dynamic SEO Metadata based on locale & new positioning
-        $title = __('landing-v3.seo.title');
-        $description = __('landing-v3.seo.description');
+        $title = __('landing.seo.title');
+        $description = __('landing.seo.description');
 
         SEOMeta::setTitle($title);
         SEOMeta::setDescription($description);
@@ -63,6 +63,6 @@ class LandingController extends Controller
             ->get()
             ->groupBy('category');
 
-        return view('landing.v3.layout', compact('packages', 'featuresByCategory'));
+        return view('landing.new', compact('packages', 'featuresByCategory'));
     }
 }
